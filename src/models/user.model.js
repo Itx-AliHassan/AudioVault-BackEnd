699284dcf06c13b41779a3c2379588a304291555
Ali Hassan <itx.alihassanch@gmail.com>
2026-09-14 23:29:57 +0500

    modifie the user.model.js schema

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true, },
-    fullName: { type: String, required: true, },
-    email: { type: String, required: true, unique: true, },
-    password: { type: String, required: true, },
+    username: { type: String, required: [true, "Username is required to create an account"], unique: [true, "Username must be unique to create an account this username is already register"], },
+    fullName: { type: String, required: [true, "fullname is required to create an account"], },
+    email: { type: String, required: [true, "A email is required to create an account"], unique: [true, "Email should be unique to create an account this email is already register"], },
+    password: { type: String, required: [true, "Password is required to create an account"], select: false, },
     role: { type: String, enum: ['user', 'admin'], default: 'user' }
 }, { timestamps: true });
 
