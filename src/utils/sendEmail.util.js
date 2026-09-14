32906f1a99dbc2bc070580c990013f36f9afc24d
Ali Hassan <itx.alihassanch@gmail.com>
2026-09-15 00:25:28 +0500

    modifie the sendEmail.util.js code switch from resend to nodemail

--- src/utils/sendEmail.util.js
+++ src/utils/sendEmail.util.js
@@ -1,5 +1,21 @@
-const sendEmail = async () => {
+const nodemailer = require('nodemailer');
 
-}
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
+});
 
-module.exports = sendEmail;
+const accountCreate = async (user) => { }
+
+const accountLogin = async (user) => { }
+
+const accountForgotPassword = async (user) => { }
+
+const emailVerification = async (user) => { }
+
+const songUpload = async (user) => { }
+
+module.exports = { accountCreate, accountLogin, accountForgotPassword, emailVerification, songUpload };
